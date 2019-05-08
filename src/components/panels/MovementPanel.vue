6f64633723a96c6e9f4ff4899941495451329310
danielfmo <danielfilipe.mo@gmail.com>
2019-05-08 21:20:10 +0200

    Change movement buttons hidden conditions

--- src/components/panels/MovementPanel.vue
+++ src/components/panels/MovementPanel.vue
@@ -13,53 +13,67 @@
 <template>
 	<v-card>
 		<v-card-title class="pt-2 pb-0">
-			<code-btn color="primary" small code="G28" :title="$t('button.home.titleAll')" class="ml-0 hidden-sm-and-down">
-				{{ $t('button.home.captionAll') }}
-			</code-btn>
+			<code-btn
+				color="primary"
+				small
+				code="G28"
+				:title="$t('button.home.titleAll')"
+				class="ml-0 hidden-sm-and-down"
+			>{{ $t('button.home.captionAll') }}</code-btn>
 
 			<v-spacer class="hidden-sm-and-down"></v-spacer>
 
-			<v-icon small class="mr-1">swap_horiz</v-icon> {{ $t('panel.movement.caption') }}
-
+			<v-icon small class="mr-1">swap_horiz</v-icon>
+			{{ $t('panel.movement.caption') }}
 			<v-spacer></v-spacer>
 
 			<v-menu offset-y left :disabled="uiFrozen" v-tab-control>
 				<template slot="activator">
 					<v-btn color="primary" small class="mx-0" :disabled="uiFrozen">
-						{{ $t('panel.movement.compensation') }} <v-icon>arrow_drop_down</v-icon>
+						{{ $t('panel.movement.compensation') }}
+						<v-icon>arrow_drop_down</v-icon>
 					</v-btn>
 				</template>
 
 				<v-card>
 					<v-list>
 						<template v-if="move.compensation">
-							<v-list-tile class="center-menu-item">
-								{{ $t('panel.movement.compensationInUse', [move.compensation]) }}
-							</v-list-tile>
+							<v-list-tile
+								class="center-menu-item"
+							>{{ $t('panel.movement.compensationInUse', [move.compensation]) }}</v-list-tile>
 
 							<v-divider></v-divider>
 						</template>
 
 						<v-list-tile @click="sendCode('G32')">
-							<v-icon class="mr-1">view_module</v-icon> {{ $t(move.geometry.type === 'delta' ? 'panel.movement.runDelta' : 'panel.movement.runBed') }}
+							<v-icon class="mr-1">view_module</v-icon>
+							{{ $t(move.geometry.type === 'delta' ? 'panel.movement.runDelta' : 'panel.movement.runBed') }}
 						</v-list-tile>
-						<v-list-tile :disabled="!move.compensation || move.compensation.indexOf('Point') === -1" @click="sendCode('M561')">
-							<v-icon class="mr-1">clear</v-icon> {{ $t('panel.movement.disableBedCompensation') }} 
+						<v-list-tile
+							:disabled="!move.compensation || move.compensation.indexOf('Point') === -1"
+							@click="sendCode('M561')"
+						>
+							<v-icon class="mr-1">clear</v-icon>
+							{{ $t('panel.movement.disableBedCompensation') }}
 						</v-list-tile>
 
 						<v-divider></v-divider>
 
 						<v-list-tile @click="sendCode('G29')">
-							<v-icon class="mr-1">grid_on</v-icon> {{ $t('panel.movement.runMesh') }}
+							<v-icon class="mr-1">grid_on</v-icon>
+							{{ $t('panel.movement.runMesh') }}
 						</v-list-tile>
 						<v-list-tile @click="showMeshEditDialog = true">
-							<v-icon class="mr-1">edit</v-icon> {{ $t('panel.movement.editMesh') }}
+							<v-icon class="mr-1">edit</v-icon>
+							{{ $t('panel.movement.editMesh') }}
 						</v-list-tile>
 						<v-list-tile @click="sendCode('G29 S1')">
-							<v-icon class="mr-1">save</v-icon> {{ $t('panel.movement.loadMesh') }}
+							<v-icon class="mr-1">save</v-icon>
+							{{ $t('panel.movement.loadMesh') }}
 						</v-list-tile>
 						<v-list-tile :disabled="move.compensation !== 'Mesh'" @click="sendCode('G29 S2')">
-							<v-icon class="mr-1">grid_off</v-icon> {{ $t('panel.movement.disableMeshCompensation') }}
+							<v-icon class="mr-1">grid_off</v-icon>
+							{{ $t('panel.movement.disableMeshCompensation') }}
 						</v-list-tile>
 					</v-list>
 				</v-card>
@@ -70,15 +84,21 @@
 			<!-- Mobile home buttons -->
 			<v-layout justify-center row wrap class="hidden-md-and-up">
 				<v-flex>
-					<code-btn color="primary" code="G28" :title="$t('button.home.titleAll')" block>
-						{{ $t('button.home.captionAll') }}
-					</code-btn>
+					<code-btn
+						color="primary"
+						code="G28"
+						:title="$t('button.home.titleAll')"
+						block
+					>{{ $t('button.home.captionAll') }}</code-btn>
 				</v-flex>
 				<v-flex v-for="axis in displayedAxes" :key="axis.letter">
-					<code-btn :color="axis.homed ? 'primary' : 'warning'" :disabled="uiFrozen" :title="$t('button.home.title', [axis.letter])" :code="`G28 ${axis.letter}`" block>
-
-						{{ $t('button.home.caption', [axis.letter]) }}
-					</code-btn>
+					<code-btn
+						:color="axis.homed ? 'primary' : 'warning'"
+						:disabled="uiFrozen"
+						:title="$t('button.home.title', [axis.letter])"
+						:code="`G28 ${axis.letter}`"
+						block
+					>{{ $t('button.home.caption', [axis.letter]) }}</code-btn>
 				</v-flex>
 			</v-layout>
 
@@ -87,10 +107,13 @@
 				<v-flex shrink class="hidden-sm-and-down">
 					<v-layout column>
 						<v-flex v-for="axis in displayedAxes" :key="axis.letter">
-							<code-btn :color="axis.homed ? 'primary' : 'warning'" :disabled="uiFrozen" :title="$t('button.home.title', [axis.letter])" :code="`G28 ${axis.letter}`" class="ml-0">
-
-								{{ $t('button.home.caption', [axis.letter]) }}
-							</code-btn>
+							<code-btn
+								:color="axis.homed ? 'primary' : 'warning'"
+								:disabled="uiFrozen"
+								:title="$t('button.home.title', [axis.letter])"
+								:code="`G28 ${axis.letter}`"
+								class="ml-0"
+							>{{ $t('button.home.caption', [axis.letter]) }}</code-btn>
 						</v-flex>
 					</v-layout>
 				</v-flex>
@@ -105,8 +128,15 @@
 								<v-flex v-for="index in numMoveSteps" :key="-index" :class="getMoveCellClass(index - 1)">
 									<v-layout column>
 										<v-flex v-for="axis in displayedAxes" :key="axis.letter">
-											<code-btn :code="`G91\nG1 ${axis.letter}${-moveSteps(axis.letter)[index - 1]} F${Math.round(moveFeedrate * 60)}\nG90`" no-wait @contextmenu.prevent="showMoveStepDialog(axis.letter, index - 1)" block class="move-btn">
-												<v-icon>keyboard_arrow_left</v-icon> {{ axis.letter + -moveSteps(axis.letter)[index - 1] }}
+											<code-btn
+												:code="`G91\nG1 ${axis.letter}${-moveSteps(axis.letter)[index - 1]} F${Math.round(moveFeedrate * 60)}\nG90`"
+												no-wait
+												@contextmenu.prevent="showMoveStepDialog(axis.letter, index - 1)"
+												block
+												class="move-btn"
+											>
+												<v-icon>keyboard_arrow_left</v-icon>
+												{{ axis.letter + -moveSteps(axis.letter)[index - 1] }}
 											</code-btn>
 										</v-flex>
 									</v-layout>
@@ -119,11 +149,22 @@
 						<!-- Increasing movements -->
 						<v-flex>
 							<v-layout row>
-								<v-flex v-for="index in numMoveSteps" :key="index" :class="getMoveCellClass(numMoveSteps - index)">
+								<v-flex
+									v-for="index in numMoveSteps"
+									:key="index"
+									:class="getMoveCellClass(numMoveSteps - index)"
+								>
 									<v-layout column>
 										<v-flex v-for="axis in displayedAxes" :key="axis.letter">
-											<code-btn :code="`G91\nG1 ${axis.letter}${moveSteps(axis.letter)[numMoveSteps - index]} F${Math.round(moveFeedrate * 60)}\nG90`" no-wait @contextmenu.prevent="showMoveStepDialog(axis.letter, numMoveSteps - index)" block class="move-btn">
-												{{ axis.letter + '+' + moveSteps(axis.letter)[numMoveSteps - index] }} <v-icon>keyboard_arrow_right</v-icon>
+											<code-btn
+												:code="`G91\nG1 ${axis.letter}${moveSteps(axis.letter)[numMoveSteps - index]} F${Math.round(moveFeedrate * 60)}\nG90`"
+												no-wait
+												@contextmenu.prevent="showMoveStepDialog(axis.letter, numMoveSteps - index)"
+												block
+												class="move-btn"
+											>
+												{{ axis.letter + '+' + moveSteps(axis.letter)[numMoveSteps - index] }}
+												<v-icon>keyboard_arrow_right</v-icon>
 											</code-btn>
 										</v-flex>
 									</v-layout>
@@ -136,25 +177,28 @@
 		</v-card-text>
 
 		<mesh-edit-dialog :shown.sync="showMeshEditDialog"></mesh-edit-dialog>
-		<input-dialog :shown.sync="moveStepDialog.shown" :title="$t('dialog.changeMoveStep.title')" :prompt="$t('dialog.changeMoveStep.prompt')" :preset="moveStepDialog.preset" is-numeric-value @confirmed="moveStepDialogConfirmed"></input-dialog>
+		<input-dialog
+			:shown.sync="moveStepDialog.shown"
+			:title="$t('dialog.changeMoveStep.title')"
+			:prompt="$t('dialog.changeMoveStep.prompt')"
+			:preset="moveStepDialog.preset"
+			is-numeric-value
+			@confirmed="moveStepDialogConfirmed"
+		></input-dialog>
 
 		<v-alert v-if="unhomedAxes.length" :value="true" type="warning">
 			{{ $tc('panel.movement.axesNotHomed', unhomedAxes.length) }}
-			<strong>
-				{{ unhomedAxes.map(axis => axis.letter).reduce((a, b) => `${a}, ${b}`) }}
-			</strong>
+			<strong>{{ unhomedAxes.map(axis => axis.letter).reduce((a, b) => `${a}, ${b}`) }}</strong>
 		</v-alert>
 
-		<v-alert :value="!move.axes.length" type="info">
-			{{ $t('panel.movement.noAxes') }}
-		</v-alert>
+		<v-alert :value="!move.axes.length" type="info">{{ $t('panel.movement.noAxes') }}</v-alert>
 	</v-card>
 </template>
 
 <script>
-'use strict'
+'use strict';
 
-import { mapState, mapGetters, mapActions, mapMutations } from 'vuex'
+import { mapState, mapGetters, mapActions, mapMutations } from 'vuex';
 
 export default {
 	computed: {
@@ -162,8 +206,12 @@ export default {
 		...mapState('machine/model', ['move']),
 		...mapState('machine/settings', ['moveFeedrate']),
 		...mapGetters('machine/settings', ['moveSteps', 'numMoveSteps']),
-		displayedAxes() { return this.move.axes.filter(axis => axis.visible); },
-		unhomedAxes() { return this.move.axes.filter(axis => axis.visible && !axis.homed); }
+		displayedAxes() {
+			return this.move.axes.filter(axis => axis.visible);
+		},
+		unhomedAxes() {
+			return this.move.axes.filter(axis => axis.visible && !axis.homed);
+		}
 	},
 	data() {
 		return {
@@ -175,17 +223,17 @@ export default {
 				index: 0,
 				preset: 0
 			}
-		}
+		};
 	},
 	methods: {
 		...mapActions('machine', ['sendCode']),
 		...mapMutations('machine/settings', ['setMoveStep']),
 		getMoveCellClass(index) {
 			let classes = '';
-			if (index === 0 || index === 5) {
+			if (index === 4 || index === 5) {
 				classes += 'hidden-lg-and-down';
 			}
-			if (index > 1 && index < 4 && index % 2 === 1) {
+			if (index === 3) {
 				classes += 'hidden-md-and-down';
 			}
 			return classes;
@@ -207,5 +255,5 @@ export default {
 			this.moveStepDialog.shown = false;
 		}
 	}
-}
+};
 </script>
